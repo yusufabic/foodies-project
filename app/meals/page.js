17c9febad763dd5b19d69e37f1dfabc597b26016
yusufabic <yusuf.abic@outlook.com>
2024-04-22 13:37:56 +0300

    fetching data by leveraging nextJS &fFullstack capabilities

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,43 +1,27 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
 import Link from "next/link";
+import { GetMeals } from "@/lib/meal";
 
-export default function Meals() {
+export default async function MealsPage() {
+  const meals = await GetMeals();
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>Meals </h1>
-      <p style={{ textAlign: "center" }}>
-        <Link
-          href="/meals/share"
-          style={{ color: "white", textAlign: "center", fontSize: "20px" }}
-        >
-          Share
-        </Link>
-      </p>
-
-      <p style={{ textAlign: "center" }}>
-        <Link
-          href="/meals/detail-1"
-          style={{ color: "white", textAlign: "center", fontSize: "20px" }}
-        >
-          Detail-1
-        </Link>
-      </p>
-      <p style={{ textAlign: "center" }}>
-        <Link
-          href="/meals/detail-2"
-          style={{ color: "white", textAlign: "center", fontSize: "20px" }}
-        >
-          Detail-2
-        </Link>
-      </p>
-
-      <p style={{ textAlign: "center" }}>
-        <Link
-          href="/"
-          style={{ color: "white", textAlign: "center", fontSize: "20px" }}
-        >
-          Comeback
-        </Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious melas, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. it is easy and fan
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
   );
 }
