364a3bbb071477fbda32da6b3afe43ea8eca98a1
yusufabic <yusuf.abic@outlook.com>
2024-05-01 08:34:49 +0300

    meal share page and form upload created

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,15 +1,39 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
   const imageInputRef = useRef();
   function handlePickClick() {
     imageInputRef.current.click();
+  }
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
   }
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user"
+              fill
+            />
+          )}
+        </div>
         <input
           type="file"
           id={name}
@@ -17,6 +41,8 @@ export default function ImagePicker({ label, name }) {
           name={name}
           className={classes.input}
           ref={imageInputRef}
+          onChange={handleImageChange}
+          required
         ></input>
         <button
           className={classes.button}
